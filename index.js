935b994c4bfc8ccdc117a67ff25f2083928569f8
erwanriou <rr.1.riou@gmail.com>
2021-11-15 21:35:18 +0100

    [Project] - initiate repository2

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const http = require("http");
+const path = require("path");
+
+// Initialize express
+const app = express();
+const server = http.Server(app);
+
+// Server static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+// Listen Server
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`server running on ${port}`));
