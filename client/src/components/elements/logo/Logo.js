2a2f2205028adc7b4b37c2df99d096d68931e6ad
Isela <isela.v.alarcon@gmail.com>
2021-11-20 21:45:00 +0100

    fixing new logo

--- client/src/components/elements/logo/Logo.js
+++ client/src/components/elements/logo/Logo.js
@@ -3,7 +3,9 @@ import React from "react"
 import { LogoStyle } from "./Logo.styles"
 
 const Logo = () => {
-  return <LogoStyle src="https://ik.imagekit.io/bypiuaav6q8/IRLANDA_LOZANO_STUDIO/Logo_IL_Blanco_HKXLzp5kQz.png?updatedAt=1637263247620" alt="logo" />
+  return (
+    <LogoStyle src="https://ik.imagekit.io/bypiuaav6q8/IRLANDA_LOZANO_STUDIO/irlandalozanoSTUDIO_f6pCPqFz_4.png?updatedAt=1637441028428" alt="logo" />
+  )
 }
 
 export default Logo
